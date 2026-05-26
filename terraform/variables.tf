de375c0f70c9ddc3c61823ef0c08d3de1cb2934a
Kunal Yeole <kunalpatil9324@gmail.com>
2026-05-26 15:40:28 +0530

    variables.tf file commited

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,28 @@
 #this file is created for variables
 
+# variable "acct-name" {
+#     type = string
+# }
+# variable "replication-type" {
+#     type = string
+  
+# }
 
-variable "acct-name" {
+variable "nsg-name" {
     type = string
 }
-variable "replication-type" {
+variable "vnet-name" {
     type = string
-  
+}
+variable "vnet-ip" {
+    type = string
+}
+variable "subnet-name" {
+    type = string
+}
+variable "subnet-ip" {
+    type = string
+}
+variable "environment-name" {
+    type = string
 }
