177b7d9fb7f0f21baeeb5ab63aa091cc45797efd
Kunal Yeole <kunalpatil9324@gmail.com>
2026-05-26 15:39:44 +0530

    main.tf modules file commited

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,15 +19,25 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "rg-block" {
-  name = "storage-rg-${var.acct-name}"
-  location = "southindia" 
-}
-resource "azurerm_storage_account" "storage-rg" {
-  name = var.acct-name
-  location = azurerm_resource_group.rg-block.location
-  resource_group_name = azurerm_resource_group.rg-block.name
-  account_tier = "Standard"
-  account_replication_type = var.replication-type
+# resource "azurerm_resource_group" "rg-block" {
+#   name = "storage-rg-${var.acct-name}"
+#   location = "southindia" 
+# }
+# resource "azurerm_storage_account" "storage-rg" {
+#   name = var.acct-name
+#   location = azurerm_resource_group.rg-block.location
+#   resource_group_name = azurerm_resource_group.rg-block.name
+#   account_tier = "Standard"
+#   account_replication_type = var.replication-type
+# }
+
+module "vnet-block" {
+  source = "../vnet-modules"
+  nsg-name = var.nsg-name
+  vnet-name = var.vnet-name
+  vnet-ip = var.vnet-ip
+  subnet-name = var.subnet-name
+  subnet-ip = var.subnet-ip
+  environment-name = var.environment-name
   
 }
