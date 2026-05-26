dee8a392a29bcc585f3d99929cd4bdb1b8585f12
Kunal Yeole <kunalpatil9324@gmail.com>
2026-05-26 12:45:55 +0530

    running pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,9 +18,52 @@ provider "azurerm" {
   # Configuration options
 }
 
+# Defining the backend state file for dev , test , prod env :
+
+terraform {
+    backend "azurerm" {
+
+        resource_group_name = "backend-storage-acct"
+        storage_account_name = "terraformstatefile2605"
+        container_name = "tfstate"
+        key = "dev.tfstate"
+      
+    }
+}
+
+terraform {
+    backend "azurerm" {
+
+        resource_group_name = "backend-storage-acct"
+        storage_account_name = "terraformstatefile2605"
+        container_name = "tfstate"
+        key = "test.tfstate"
+      
+    }
+    
+}
+
+terraform {
+    backend "azurerm" {
+
+        resource_group_name = "backend-storage-acct"
+        storage_account_name = "terraformstatefile2605"
+        container_name = "tfstate"
+        key = "prod.tfstate"
+      
+    }
+    
+}
 
 resource "azurerm_resource_group" "rg-block" {
-  name = "rg-${var.name-env}"
-  location = var.location
+  name = "storage-rg"
+  location = "southindia" 
+}
+resource "azurerm_storage_account" "storage-rg" {
+  name = var.acct-name
+  location = azurerm_resource_group.rg-block.location
+  resource_group_name = azurerm_resource_group.rg-block.name
+  account_tier = "Standard"
+  account_replication_type = var.replication-type
   
 }
