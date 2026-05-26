36d16b29c93637ac78adda794f892e9409ec2c96
Kunal Yeole <kunalpatil9324@gmail.com>
2026-05-26 18:54:39 +0530

    keys changed for all env

--- backend/prod.state.tf
+++ backend/prod.state.tf
@@ -5,7 +5,7 @@ terraform {
 
         resource_group_name = "backend-storage-acct"
         storage_account_name = "terraformstatefile2605"
-        container_name = "tfstate"
+        container_name = "backend-container"
         key = "prod.tfstate"
         
       
